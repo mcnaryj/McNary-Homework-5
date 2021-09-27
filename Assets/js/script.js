78307c81912ceea2c668fe03bb69947b20d215e3
Jack McNary <jack.mcnary@gmail.com>
2021-09-26 19:47:26 -0600

    More changes

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,73 +1,104 @@
 // Created variables (indexed at 0, one for each hour of the day) that can be stored/looped through the scheduler
+// notes field to contain text input
 var planDay = [
     {
         id: 0,
         hour: "09",
         time: "09",
         meridian: "am",
-        reminder: "",
+        notes: "",
     },
     {
         id: 1,
         hour: "10",
         time: "10",
         meridian: "am",
-        reminder: "",
+        notes: "",
     },
     {
         id: 2,
         hour: "11",
         time: "11",
         meridian: "am",
-        reminder: "",
+        notes: "",
     },
     {
         id: 3,
         hour: "12",
         time: "12",
         meridian: "pm",
-        reminder: "",
+        notes: "",
     },
     {
         id: 4,
         hour: "01",
         time: "01",
         meridian: "pm",
-        reminder: "",
+        notes: "",
     },
     {
         id: 5,
         hour: "02",
         time: "02",
         meridian: "pm",
-        reminder: "",
+        notes: "",
     },
     {
         id: 6,
         hour: "03",
         time: "03",
         meridian: "pm",
-        reminder: "",
+        notes: "",
     },
     {
         id: 7,
         hour: "04",
         time: "04",
         meridian: "pm",
-        reminder: "",
+        notes: "",
     }, {
         id: 8,
         hour: "05",
         time: "05",
         meridian: "pm",
-        reminder: "",
+        notes: "",
     },
 ]
-// Logged to the console to display the objects as a string
-console.log("Plan Day" + JSON.stringify(planDay));
+// Logged to the console to display the array as a JSON string
+console.log("Day planner: " + JSON.stringify(planDay));
 
-function currenttHeaderDate() {
-    var currenttHeaderDate = moment.format('dddd, MMMM, do');
-    $("#currentDay").text(currenttHeaderDate);
+// Created function to save these values in the local storage
+function savePlans() {
+    localStorage.setItem("planDay", JSON.stringify(planDay))
+}
+// Added a console log to get the stored items, at the present it reads null 
+console.log("Planned Day: " + JSON.parse(localStorage.getItem("Planned Day")));
+
+// Creating a for loop that checks for content entered for the hours specified in the array
+function showPlans() {
+    console.log("something doing?");
+    planDay.forEach(function (currentHour) {
+        $(`#${currentHour.id}`).val(currentHour.notes);
+    })
 
 }
+
+
+// 
+function currentDate() {
+    var currentDate = moment().format('dddd, MMMM, Do');
+    $("#currentDay").text(currentDate);
+}
+
+function getStorage() {
+    var plannedDay = JSON.parse(localStorage.getItem("Planned Day"));
+
+    if (plannedDay) {
+        planDay = plannedDay
+    }
+    savePlans;
+    showPlans();
+}
+// Calling the function
+currentDate();
+
